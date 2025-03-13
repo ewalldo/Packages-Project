24a930d7a0d77510122f7b727bb04f6c138d5a5a
Evaldo Borba <evaldo.lborba@gmail.com>
2025-03-13 17:10:08 +0900

    - Add new extensions for measurements conversion
    - Add new extensions for measurements conversion

--- Assets/Extensions/README.md
+++ Assets/Extensions/README.md
@@ -83,6 +83,14 @@
   - [Measurements](#measurementExtensions)
     - [FromMeters](#measurementExtensionsFromMeters)
     - [ToMeters](#measurementExtensionsToMeters)
+    - [FromCelsius](#measurementExtensionsFromCelsius)
+    - [ToCelsius](#measurementExtensionsToCelsius)
+    - [FromKilograms](#measurementExtensionsFromKilograms)
+    - [ToKilograms](#measurementExtensionsToKilograms)
+    - [FromMetersSecond](#measurementExtensionsFromMetersSecond)
+    - [ToMetersSecond](#measurementExtensionsToMetersSecond)
+    - [FromSeconds](#measurementExtensionsFromSeconds)
+    - [ToSeconds](#measurementExtensionsToSeconds)
   - [Renderer](#rendererExtensions)
     - [IsVisibleFrom](#rendererExtensionsIsVisibleFrom)
   - [RichText](#richtextExtensions)
@@ -1190,6 +1198,134 @@ float ToMeters(LengthUnitType lengthUnitType);
 | Type | Description |
 | :--- | :--- |
 | float | Float value representing the distance in meters |
+
+
+#### FromCelsius <a name="measurementExtensionsFromCelsius"/>
+Converts a float value from celsius to a specific unit of temperature
+#### Declaration
+```csharp
+float FromCelsius(TemperatureUnitType temperatureUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| TemperatureUnitType | temperatureUnitType | The unit of temperature to convert to |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the temperature in the desired unit |
+
+
+#### ToCelsius <a name="measurementExtensionsToCelsius"/>
+Converts a float value from a specific unit of temperature to celsius
+#### Declaration
+```csharp
+float ToCelsius(TemperatureUnitType temperatureUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| TemperatureUnitType | temperatureUnitType | The unit of temperature to convert from |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the temperature in celsius |
+
+
+#### FromKilograms <a name="measurementExtensionsFromKilograms"/>
+Converts a float value from kilograms to a specific unit of mass
+#### Declaration
+```csharp
+float FromKilograms(MassUnitType massUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| MassUnitType | massUnitType | The unit of mass to convert to |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the mass in the desired unit |
+
+
+#### ToKilograms <a name="measurementExtensionsToKilograms"/>
+Converts a float value from a specific unit of mass to kilograms
+#### Declaration
+```csharp
+float ToKilograms(MassUnitType massUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| MassUnitType | massUnitType | The unit of mass to convert from |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the mass in kilograms |
+
+
+#### FromMetersSecond <a name="measurementExtensionsFromMetersSecond"/>
+Converts a float value from meters per second to a specific unit of speed
+#### Declaration
+```csharp
+float FromMetersSecond(SpeedUnitType speedUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| SpeedUnitType | speedUnitType | The unit of speed to convert to |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the speed in the desired unit |
+
+
+#### ToMetersSecond <a name="measurementExtensionsToMetersSecond"/>
+Converts a float value from a specific unit of speed to meters per second
+#### Declaration
+```csharp
+float ToMetersSecond(SpeedUnitType speedUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| SpeedUnitType | speedUnitType | The unit of speed to convert from |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the speed in meters per second |
+
+
+#### FromSeconds <a name="measurementExtensionsFromSeconds"/>
+Converts a float value from seconds to a specific unit of time
+#### Declaration
+```csharp
+float FromSeconds(TimeUnitType timeUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| TimeUnitType | timeUnitType | The unit of time to convert to |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the time in the desired unit |
+
+
+#### ToSeconds <a name="measurementExtensionsToSeconds"/>
+Converts a float value from a specific unit of time to seconds
+#### Declaration
+```csharp
+float ToSeconds(TimeUnitType timeUnitType);
+```
+#### Parameters
+| Type | Name | Description |
+| :--- | :--- | :--- |
+| TimeUnitType | timeUnitType | The unit of time to convert from |
+#### Returns
+| Type | Description |
+| :--- | :--- |
+| float | Float value representing the time in seconds |
 
 
 ### 5.10 Renderer Extensions <a name="rendererExtensions"/>
--- Assets/Extensions/Scripts/MeasurementExtensions.cs
+++ Assets/Extensions/Scripts/MeasurementExtensions.cs
@@ -38,27 +38,18 @@ namespace Extensions
         /// <returns>Float value representing the distance in the desired unit of length</returns>
         public static float FromMeters(this float length, LengthUnitType lengthUnitType)
         {
-            switch (lengthUnitType)
+            return lengthUnitType switch
             {
-                case LengthUnitType.Meter:
-                    return length / METER_VALUE;
-                case LengthUnitType.Centimeter:
-                    return length / CENTIMETER_VALUE;
-                case LengthUnitType.Millimeter:
-                    return length / MILLIMETER_VALUE;
-                case LengthUnitType.Kilometer:
-                    return length / KILOMETER_VALUE;
-                case LengthUnitType.Inch:
-                    return length / INCH_VALUE;
-                case LengthUnitType.Foot:
-                    return length / FOOT_VALUE;
-                case LengthUnitType.Yard:
-                    return length / YARD_VALUE;
-                case LengthUnitType.Mile:
-                    return length / MILE_VALUE;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(lengthUnitType), "Unsupported unit of lengths.");
-            }
+                LengthUnitType.Meter => length / METER_VALUE,
+                LengthUnitType.Centimeter => length / CENTIMETER_VALUE,
+                LengthUnitType.Millimeter => length / MILLIMETER_VALUE,
+                LengthUnitType.Kilometer => length / KILOMETER_VALUE,
+                LengthUnitType.Inch => length / INCH_VALUE,
+                LengthUnitType.Foot => length / FOOT_VALUE,
+                LengthUnitType.Yard => length / YARD_VALUE,
+                LengthUnitType.Mile => length / MILE_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(lengthUnitType), "Unsupported unit of length."),
+            };
         }
 
         /// <summary>
@@ -69,27 +60,236 @@ namespace Extensions
         /// <returns>Float value representing the distance in meters</returns>
         public static float ToMeters(this float length, LengthUnitType lengthUnitType)
         {
-            switch (lengthUnitType)
+            return lengthUnitType switch
             {
-                case LengthUnitType.Meter:
-                    return length * METER_VALUE;
-                case LengthUnitType.Centimeter:
-                    return length * CENTIMETER_VALUE;
-                case LengthUnitType.Millimeter:
-                    return length * MILLIMETER_VALUE;
-                case LengthUnitType.Kilometer:
-                    return length * KILOMETER_VALUE;
-                case LengthUnitType.Inch:
-                    return length * INCH_VALUE;
-                case LengthUnitType.Foot:
-                    return length * FOOT_VALUE;
-                case LengthUnitType.Yard:
-                    return length * YARD_VALUE;
-                case LengthUnitType.Mile:
-                    return length * MILE_VALUE;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(lengthUnitType), "Unsupported unit of lengths.");
-            }
+                LengthUnitType.Meter => length * METER_VALUE,
+                LengthUnitType.Centimeter => length * CENTIMETER_VALUE,
+                LengthUnitType.Millimeter => length * MILLIMETER_VALUE,
+                LengthUnitType.Kilometer => length * KILOMETER_VALUE,
+                LengthUnitType.Inch => length * INCH_VALUE,
+                LengthUnitType.Foot => length * FOOT_VALUE,
+                LengthUnitType.Yard => length * YARD_VALUE,
+                LengthUnitType.Mile => length * MILE_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(lengthUnitType), "Unsupported unit of length."),
+            };
+        }
+        #endregion
+
+        #region Temperature
+        private static readonly float KELVIN_VALUE = 273.15f;
+
+        public enum TemperatureUnitType
+        {
+            Celsius,
+            Fahrenheit,
+            Kelvin
+        }
+
+        /// <summary>
+        /// Converts a float value from celsius to a specific unit of temperature
+        /// </summary>
+        /// <param name="temperature">The temperature in celsius to convert from</param>
+        /// <param name="temperatureUnitType">The unit of temperature to convert to</param>
+        /// <returns>Float value representing the temperature in the desired unit</returns>
+        public static float FromCelsius(this float temperature, TemperatureUnitType temperatureUnitType)
+        {
+            return temperatureUnitType switch
+            {
+                TemperatureUnitType.Celsius => temperature,
+                TemperatureUnitType.Fahrenheit => (temperature * 9 / 5f) + 32,
+                TemperatureUnitType.Kelvin => temperature + KELVIN_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(temperatureUnitType), "Unsupported unit of temperature."),
+            };
+        }
+
+        /// <summary>
+        /// Converts a float value from a specific unit of temperature to celsius
+        /// </summary>
+        /// <param name="temperature">The temperature to convert from</param>
+        /// <param name="temperatureUnitType">The unit of temperature to convert from</param>
+        /// <returns>Float value representing the temperature in celsius</returns>
+        public static float ToCelsius(this float temperature, TemperatureUnitType temperatureUnitType)
+        {
+            return temperatureUnitType switch
+            {
+                TemperatureUnitType.Celsius => temperature,
+                TemperatureUnitType.Fahrenheit => (temperature - 32) * 5 / 9f,
+                TemperatureUnitType.Kelvin => temperature - KELVIN_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(temperatureUnitType), "Unsupported unit of temperature."),
+            };
+        }
+        #endregion
+
+        #region Mass
+        private static readonly float KILOGRAM_VALUE = 1f;
+        private static readonly float GRAM_VALUE = 1000f;
+        private static readonly float MILLIGRAM_VALUE = 1000000f;
+        private static readonly float TONNE_VALUE = 0.001f;
+        private static readonly float POUND_VALUE = 2.2046226218f;
+        private static readonly float OUNCE_VALUE = 35.27396195f;
+
+        public enum MassUnitType
+        {
+            Kilogram,
+            Gram,
+            Milligram,
+            Tonne,
+            Pound,
+            Ounce
+        }
+
+        /// <summary>
+        /// Converts a float value from kilograms to a specific unit of mass
+        /// </summary>
+        /// <param name="mass">The mass in kilograms to convert from</param>
+        /// <param name="massUnitType">The unit of mass to convert to</param>
+        /// <returns>Float value representing the weight in the desired unit of mass</returns>
+        public static float FromKilograms(this float mass, MassUnitType massUnitType)
+        {
+            return massUnitType switch
+            {
+                MassUnitType.Kilogram => mass * KILOGRAM_VALUE,
+                MassUnitType.Gram => mass * GRAM_VALUE,
+                MassUnitType.Milligram => mass * MILLIGRAM_VALUE,
+                MassUnitType.Tonne => mass * TONNE_VALUE,
+                MassUnitType.Pound => mass * POUND_VALUE,
+                MassUnitType.Ounce => mass * OUNCE_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(massUnitType), "Unsupported unit of mass."),
+            };
+        }
+
+        /// <summary>
+        /// Converts a float value from a specific unit of mass to kilograms
+        /// </summary>
+        /// <param name="mass">The weight to convert from</param>
+        /// <param name="massUnitType">The unit of mass to convert from</param>
+        /// <returns>Float value representing the weight in kilograms</returns>
+        public static float ToKilograms(this float mass, MassUnitType massUnitType)
+        {
+            return massUnitType switch
+            {
+                MassUnitType.Kilogram => mass / KILOGRAM_VALUE,
+                MassUnitType.Gram => mass / GRAM_VALUE,
+                MassUnitType.Milligram => mass / MILLIGRAM_VALUE,
+                MassUnitType.Tonne => mass / TONNE_VALUE,
+                MassUnitType.Pound => mass / POUND_VALUE,
+                MassUnitType.Ounce => mass / OUNCE_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(massUnitType), "Unsupported unit of mass."),
+            };
+        }
+        #endregion
+
+        #region Speed
+        private static readonly float METERSSECOND_VALUE = 1f;
+        private static readonly float KILOMETERSHOUR_VALUE = 3.6f;
+        private static readonly float MILESHOUR_VALUE = 2.2369362921f;
+
+        public enum SpeedUnitType
+        {
+            MetersSecond,
+            KilometersHour,
+            MilesHour,
+        }
+
+        /// <summary>
+        /// Converts a float value from meters per second to a specific unit of speed
+        /// </summary>
+        /// <param name="speed">The speed in meters per second to convert from</param>
+        /// <param name="speedUnitType">The unit of speed to convert to</param>
+        /// <returns>Float value representing the velocity in the desired unit of speed</returns>
+        public static float FromMetersSecond(this float speed, SpeedUnitType speedUnitType)
+        {
+            return speedUnitType switch
+            {
+                SpeedUnitType.MetersSecond => speed * METERSSECOND_VALUE,
+                SpeedUnitType.KilometersHour => speed * KILOMETERSHOUR_VALUE,
+                SpeedUnitType.MilesHour => speed * MILESHOUR_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(speedUnitType), "Unsupported unit of speed."),
+            };
+        }
+
+        /// <summary>
+        /// Converts a float value from a specific unit of speed to meters per second
+        /// </summary>
+        /// <param name="speed">The speed to convert from</param>
+        /// <param name="speedUnitType">The unit of speed to convert from</param>
+        /// <returns>Float value representing the speed in meters per second</returns>
+        public static float ToMetersSecond(this float speed, SpeedUnitType speedUnitType)
+        {
+            return speedUnitType switch
+            {
+                SpeedUnitType.MetersSecond => speed / METERSSECOND_VALUE,
+                SpeedUnitType.KilometersHour => speed / KILOMETERSHOUR_VALUE,
+                SpeedUnitType.MilesHour => speed / MILESHOUR_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(speedUnitType), "Unsupported unit of speed."),
+            };
+        }
+        #endregion
+
+        #region Time
+        private static readonly float SECONDS_VALUE = 1f;
+        private static readonly float MILLISECONDS_VALUE = 0.001f;
+        private static readonly float MINUTE_VALUE = 60f;
+        private static readonly float HOUR_VALUE = 3600f;
+        private static readonly float DAY_VALUE = 86400f;
+        private static readonly float WEEK_VALUE = 604800f;
+        private static readonly float MONTH_VALUE = 2628000f;
+        private static readonly float YEAR_VALUE = 31557600f;
+
+        public enum TimeUnitType
+        {
+            Seconds,
+            Milliseconds,
+            Minute,
+            Hour,
+            Day,
+            Week,
+            Month,
+            Year
+        }
+
+        /// <summary>
+        /// Converts a float value from seconds to a specific unit of time
+        /// </summary>
+        /// <param name="time">The time in seconds to convert from</param>
+        /// <param name="timeUnitType">The unit of time to convert to</param>
+        /// <returns>Float value representing the time in the desired unit of time</returns>
+        public static float FromSeconds(this float time, TimeUnitType timeUnitType)
+        {
+            return timeUnitType switch
+            {
+                TimeUnitType.Seconds => time / SECONDS_VALUE,
+                TimeUnitType.Milliseconds => time / MILLISECONDS_VALUE,
+                TimeUnitType.Minute => time / MINUTE_VALUE,
+                TimeUnitType.Hour => time / HOUR_VALUE,
+                TimeUnitType.Day => time / DAY_VALUE,
+                TimeUnitType.Week => time / WEEK_VALUE,
+                TimeUnitType.Month => time / MONTH_VALUE,
+                TimeUnitType.Year => time / YEAR_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(timeUnitType), "Unsupported unit of time."),
+            };
+        }
+
+        /// <summary>
+        /// Converts a float value from a specific unit of time to seconds
+        /// </summary>
+        /// <param name="time">The time to convert from</param>
+        /// <param name="timeUnitType">The unit of time to convert from</param>
+        /// <returns>Float value representing the time in seconds</returns>
+        public static float ToSeconds(this float time, TimeUnitType timeUnitType)
+        {
+            return timeUnitType switch
+            {
+                TimeUnitType.Seconds => time * SECONDS_VALUE,
+                TimeUnitType.Milliseconds => time * MILLISECONDS_VALUE,
+                TimeUnitType.Minute => time * MINUTE_VALUE,
+                TimeUnitType.Hour => time * HOUR_VALUE,
+                TimeUnitType.Day => time * DAY_VALUE,
+                TimeUnitType.Week => time * WEEK_VALUE,
+                TimeUnitType.Month => time * MONTH_VALUE,
+                TimeUnitType.Year => time * YEAR_VALUE,
+                _ => throw new ArgumentOutOfRangeException(nameof(timeUnitType), "Unsupported unit of time."),
+            };
         }
         #endregion
     }
