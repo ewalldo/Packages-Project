82bf8c8e7b3c521e2305299cefb75599333ca53b
Evaldo Borba <evaldo.lborba@gmail.com>
2026-03-24 09:53:31 +0900

    - Update UIToolkitExtras package to Unity 6000.3
    - Update UIToolkitExtras package to Unity 6000.3

--- Assets/UIToolkitExtras/Scripts/SpriteSheetImage.cs
+++ Assets/UIToolkitExtras/Scripts/SpriteSheetImage.cs
@@ -3,243 +3,230 @@ using UnityEngine.UIElements;
 
 namespace UIToolkitExtras
 {
-	//[UxmlElement]
-	public partial class SpriteSheetImage : Image
+    [UxmlElement]
+    public partial class SpriteSheetImage : Image
 	{
-//        [UxmlAttribute]
-//        public new Texture image
-//        {
-//            get => base.image;
-//            set => base.image = value;
-//        }
+        private SpriteSheetAnimationContainer animationFrames;
+        [UxmlAttribute]
+        public SpriteSheetAnimationContainer AnimationFrames
+        {
+            get => animationFrames;
+            set
+            {
+                if (animationFrames == value)
+                    return;
 
-//        [UxmlAttribute]
-//        public new Sprite sprite
-//        {
-//            get => base.sprite;
-//            set => base.sprite = value;
-//        }
+                animationFrames = value;
 
-//        private SpriteSheetAnimationContainer animationFrames;
-//        [UxmlAttribute]
-//        public SpriteSheetAnimationContainer AnimationFrames
-//        {
-//            get => animationFrames;
-//            set
-//            {
-//                if (animationFrames == value)
-//                    return;
+                if (panel != null && IsPlaying)
+                {
+                    StopAnimationLogic();
+                    if (AutoStart)
+                        PlayAnimationLogic();
+                }
+                else if (panel != null && AutoStart && animationFrames != null && animationFrames.Length > 0)
+                {
+                    PlayAnimationLogic();
+                }
+                else if (animationFrames == null || animationFrames.Length == 0)
+                {
+                    StopAnimationLogic();
+                }
+            }
+        }
 
-//                animationFrames = value;
+        private float framesPerSecond = 60f;
+        [UxmlAttribute]
+        public float FramesPerSecond
+        {
+            get => framesPerSecond;
+            set
+            {
+                if (Mathf.Approximately(framesPerSecond, value))
+                    return;
 
-//                if (panel != null && IsPlaying)
-//                {
-//                    StopAnimationLogic();
-//                    if (AutoStart)
-//                        PlayAnimationLogic();
-//                }
-//                else if (panel != null && AutoStart && animationFrames != null && animationFrames.Length > 0)
-//                {
-//                    PlayAnimationLogic();
-//                }
-//                else if (animationFrames == null || animationFrames.Length == 0)
-//                {
-//                    StopAnimationLogic();
-//                }
-//            }
-//        }
+                framesPerSecond = value > 0 ? value : 60f;
 
-//        private float framesPerSecond = 60f;
-//        [UxmlAttribute]
-//        public float FramesPerSecond
-//        {
-//            get => framesPerSecond;
-//            set
-//            {
-//                if (Mathf.Approximately(framesPerSecond, value))
-//                    return;
+                if (panel != null && IsPlaying)
+                {
+                    StopAnimationLogic();
+                    if (AutoStart)
+                        PlayAnimationLogic();
+                }
+            }
+        }
 
-//                framesPerSecond = value > 0 ? value : 60f;
+        [UxmlAttribute]
+        public bool Loop { get; set; } = true;
 
-//                if (panel != null && IsPlaying)
-//                {
-//                    StopAnimationLogic();
-//                    PlayAnimationLogic();
-//                }
-//            }
-//        }
+        [UxmlAttribute]
+        public bool AutoStart = true;
 
-//        [UxmlAttribute]
-//        public bool Loop { get; set; } = true;
+        private bool previewOnEditor = false;
+        [UxmlAttribute]
+        public bool PreviewOnEditor
+        {
+            get => previewOnEditor;
+            set
+            {
+                if (previewOnEditor == value)
+                    return;
+                previewOnEditor = value;
 
-//        [UxmlAttribute]
-//        public bool AutoStart = true;
+                if (panel != null)
+                {
+                    StopAnimationLogic();
+                    if (value)
+                        PlayAnimationLogic();
+                }
+            }
+        }
 
-//        private bool previewOnEditor = false;
-//        [UxmlAttribute]
-//        public bool PreviewOnEditor
-//        {
-//            get => previewOnEditor;
-//            set
-//            {
-//                if (previewOnEditor == value)
-//                    return;
-//                previewOnEditor = value;
+        private IVisualElementScheduledItem animationTask;
+        private int currentFrame = 0;
+        private bool isAttached = false;
 
-//                if (panel != null)
-//                {
-//                    StopAnimationLogic();
-//                    if (value)
-//                        PlayAnimationLogic();
-//                }
-//            }
-//        }
+        public SpriteSheetImage()
+            : base()
+        {
+            RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+            RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanel);
+        }
 
-//        private IVisualElementScheduledItem animationTask;
-//        private int currentFrame = 0;
-//        private bool isAttached = false;
+        public void Play()
+        {
+            if (!isAttached)
+                return;
 
-//        public SpriteSheetImage()
-//            : base()
-//        {
-//            RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
-//            RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanel);
-//        }
+            AutoStart = true;
+            PlayAnimationLogic();
+        }
 
-//        public void Play()
-//        {
-//            if (!isAttached)
-//                return;
+        public void Pause()
+        {
+            animationTask?.Pause();
+        }
 
-//            AutoStart = true;
-//            PlayAnimationLogic();
-//        }
+        public void Resume()
+        {
+            if (animationTask != null && !animationTask.isActive)
+            {
+                animationTask.Resume();
+            }
+            else if (IsReadyToPlay() && (animationTask == null || !animationTask.isActive))
+            {
+                Play();
+            }
+        }
 
-//        public void Pause()
-//        {
-//            animationTask?.Pause();
-//        }
+        public void Stop()
+        {
+            StopAnimationLogic();
 
-//        public void Resume()
-//        {
-//            if (animationTask != null && !animationTask.isActive)
-//            {
-//                animationTask.Resume();
-//            }
-//            else if (IsReadyToPlay() && (animationTask == null || !animationTask.isActive))
-//            {
-//                Play();
-//            }
-//        }
+            currentFrame = 0;
 
-//        public void Stop()
-//        {
-//            StopAnimationLogic();
+            if (animationFrames != null && animationFrames.Length > 0)
+                base.sprite = animationFrames.GetSprite(currentFrame);
+        }
 
-//            currentFrame = 0;
+        public bool IsPlaying => animationTask != null && animationTask.isActive;
 
-//            if (animationFrames != null && animationFrames.Length > 0)
-//                base.sprite = animationFrames.GetSprite(currentFrame);
-//        }
+        private void OnAttachToPanel(AttachToPanelEvent evt)
+        {
+            isAttached = true;
+            if (AutoStart)
+                PlayAnimationLogic();
+        }
 
-//        public bool IsPlaying => animationTask != null && animationTask.isActive;
+        private void OnDetachFromPanel(DetachFromPanelEvent evt)
+        {
+            isAttached = false;
+            StopAnimationLogic();
+        }
 
-//        private void OnAttachToPanel(AttachToPanelEvent evt)
-//        {
-//            isAttached = true;
-//            if (AutoStart)
-//                PlayAnimationLogic();
-//        }
+        private bool IsReadyToPlay()
+        {
+            return animationFrames != null && animationFrames.Sprites != null && animationFrames.Length > 0 && framesPerSecond > 0;
+        }
 
-//        private void OnDetachFromPanel(DetachFromPanelEvent evt)
-//        {
-//            isAttached = false;
-//            StopAnimationLogic();
-//        }
+        private void PlayAnimationLogic()
+        {
+            if (!isAttached)
+                return;
 
-//        private bool IsReadyToPlay()
-//        {
-//            return animationFrames != null && animationFrames.Sprites != null && animationFrames.Length > 0 && framesPerSecond > 0;
-//        }
+#if UNITY_EDITOR
+            if (!PreviewOnEditor && !Application.isPlaying)
+            {
+                StopAnimationLogic();
+                return;
+            }
+#endif
 
-//        private void PlayAnimationLogic()
-//        {
-//            if (!isAttached)
-//                return;
+            StopAnimationLogic();
 
-//#if UNITY_EDITOR
-//            if (!PreviewOnEditor && !Application.isPlaying)
-//            {
-//                StopAnimationLogic();
-//                return;
-//            }
-//#endif
+            if (!IsReadyToPlay())
+                return;
 
-//            StopAnimationLogic();
+            if (currentFrame >= animationFrames.Length || currentFrame < 0)
+                currentFrame = 0;
 
-//            if (!IsReadyToPlay())
-//                return;
+            Sprite initialSprite = animationFrames.GetSprite(currentFrame);
+            if (initialSprite != null)
+                base.sprite = initialSprite;
+            else
+            {
+                Debug.LogError($"Initial sprite at index {currentFrame} is null in {name}.");
+                return;
+            }
 
-//            if (currentFrame >= animationFrames.Length || currentFrame < 0)
-//                currentFrame = 0;
+            if (animationFrames.Length <= 1 && !Loop)
+            {
+                animationTask = null; // No animation needed for single frame
+                return;
+            }
 
-//            Sprite initialSprite = animationFrames.GetSprite(currentFrame);
-//            if (initialSprite != null)
-//                base.sprite = initialSprite;
-//            else
-//            {
-//                Debug.LogError($"Initial sprite at index {currentFrame} is null in {name}.");
-//                return;
-//            }
+            long delayMs = (long)(1000f / framesPerSecond);
+            if (delayMs <= 0)
+                delayMs = (long)1000f / 60;
 
-//            if (animationFrames.Length <= 1 && !Loop)
-//            {
-//                animationTask = null; // No animation needed for single frame
-//                return;
-//            }
+            animationTask = schedule.Execute(UpdateFrame).Every(delayMs);
+        }
 
-//            long delayMs = (long)(1000f / framesPerSecond);
-//            if (delayMs <= 0)
-//                delayMs = (long)1000f / 60;
+        private void StopAnimationLogic()
+        {
+            animationTask?.Pause();
+            animationTask = null;
+        }
 
-//            animationTask = schedule.Execute(UpdateFrame).Every(delayMs);
-//        }
+        private void UpdateFrame()
+        {
+            if (!IsReadyToPlay())
+            {
+                StopAnimationLogic();
+                return;
+            }
 
-//        private void StopAnimationLogic()
-//        {
-//            animationTask?.Pause();
-//            animationTask = null;
-//        }
+            currentFrame++;
 
-//        private void UpdateFrame()
-//        {
-//            if (!IsReadyToPlay())
-//            {
-//                StopAnimationLogic();
-//                return;
-//            }
+            if (currentFrame >= animationFrames.Length)
+            {
+                if (Loop)
+                    currentFrame = 0;
+                else
+                {
+                    currentFrame = animationFrames.Length - 1; // Stay on the last frame
+                    StopAnimationLogic();
+                }
+            }
 
-//            currentFrame++;
-
-//            if (currentFrame >= animationFrames.Length)
-//            {
-//                if (Loop)
-//                    currentFrame = 0;
-//                else
-//                {
-//                    currentFrame = animationFrames.Length - 1; // Stay on the last frame
-//                    StopAnimationLogic();
-//                }
-//            }
-
-//            Sprite nextSprite = animationFrames.GetSprite(currentFrame);
-//            if (nextSprite != null)
-//                base.sprite = nextSprite;
-//            else
-//            {
-//                Debug.LogError($"Sprite at index {currentFrame} is null in {name}. Stopping animation");
-//                StopAnimationLogic();
-//            }
-//        }
+            Sprite nextSprite = animationFrames.GetSprite(currentFrame);
+            if (nextSprite != null)
+                base.sprite = nextSprite;
+            else
+            {
+                Debug.LogError($"Sprite at index {currentFrame} is null in {name}. Stopping animation");
+                StopAnimationLogic();
+            }
+        }
     }
 }
